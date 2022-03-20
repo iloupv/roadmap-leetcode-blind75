763afc4040b80fddae506c7b9aee9e665d9fd1e5
Imanol <imanol@lopezolivasdev.com>
2022-03-20 17:39:56 +0100

    Added Search Rotated Sorted Array solution (.py)

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # roadmap-leetcode-blind75
 A roadmap for completing all Blind 75 leetcode problems list.
+# Blind 75 list
+You can find the list [here.](https://www.genome.gov/)
+
---
+++ blind75/array/search_rotated_sorted_array.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def search(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: int
+        """
+        left = 0
+        right = len(nums) - 1
+
+        while(left <= right):
+            mid = (left + right) >> 1
+            if nums[mid] == target:
+                return mid
+
+            if nums[mid] >= nums[right]:
+                if target < nums[mid] and target >= nums[left]:
+                    right = mid - 1
+                else:
+                    left = mid + 1
+            else:
+                if target > nums[mid] and target <= nums[right]:
+                    left = mid + 1
+                else:
+                    right = mid - 1
+
+        return -1
