7f570eb52e09956718b36e26294e697eb18e4823
Imanol <imanol@lopezolivasdev.com>
2022-03-23 23:15:10 +0100

    Fixed naming

--- blind75/bits/sum_two_intergers_bit.cpp
+++ blind75/bits/sum_two_intergers_bit.cpp
@@ -1,3 +1,20 @@
+/*
+Given two integers a and b, return the sum of the two integers without using the operators + and -.
+
+ 
+
+Example 1:
+
+Input: a = 1, b = 2
+Output: 3
+Example 2:
+
+Input: a = 2, b = 3
+Output: 5
+
+Author = Imanol López
+Time Complexity: O(n)
+*/
 class Solution {
 public:
     int getSum(int a, int b) {
