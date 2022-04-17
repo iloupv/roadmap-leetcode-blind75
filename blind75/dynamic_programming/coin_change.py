4f0e8d44f265bb486b1d22701fa0b8a6a51ce04e
Imanol López <imanol@lopezolivasdev.com>
2022-04-17 20:19:16 +0200

    Added Reverse Linked List solution (.py)

---
+++ blind75/dynamic_programming/coin_change.py
@@ -0,0 +1,24 @@
+'''
+Given the head of a singly linked list, reverse the list, and return the reversed list.
+
+Input: head = [1,2,3,4,5]
+Output: [5,4,3,2,1]
+
+Author: Imanol López
+Time Complexity: O(n)
+'''
+class Solution(object):
+    def reverseList(self, head):
+        """
+        :type head: ListNode
+        :rtype: ListNode
+        """
+        curr = head
+        prev = None
+        while curr:
+            aux = curr.next
+            curr.next = prev
+            prev = curr
+            curr = aux
+            
+        return prev
